877857f762c28973f10c27f40f2fbb98d06233fa
Dmitry Kochetov <kochetov6969@outlook.com>
2021-03-24 13:00:18 +0700

    Macros for serialization functions and types

---
+++ include/kt-serial/macros.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef SERIALIZE
+#define SERIALIZE serialize
+#endif
+
+#ifndef SIZE_TYPE
+#define SIZE_TYPE uint64_t
+#endif
+
+#ifndef SAVE
+#define SAVE save
+#endif
+
+#ifndef LOAD
+#define LOAD load
+#endif
