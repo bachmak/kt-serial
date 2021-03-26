f9d5bf6e9a07930f0779628dfd4cd095b570f544
Dmitry Kochetov <kochetov6969@outlook.com>
2021-03-26 09:23:46 +0700

    Pragma once to access.h and template arg renaming

--- include/kt-serial/access.h
+++ include/kt-serial/access.h
@@ -1,23 +1,25 @@
+#pragma once
+
 #include "kt-serial/macros.h"
 
 namespace KtSerial
 {
 struct Access
 {
-    template <class Archive, class T> inline static
-    auto serialize(Archive& ar, T& t) -> decltype(t.SERIALIZE(ar))
+    template <class Archive, class Type> inline static
+    auto serialize(Archive& ar, Type& t) -> decltype(t.SERIALIZE(ar))
     {
         return t.SERIALIZE(ar);
     }
 
-    template <class Archive, class T> inline static
-    auto save(Archive& ar, const T& t) -> decltype(t.SAVE(ar))
+    template <class Archive, class Type> inline static
+    auto save(Archive& ar, const Type& t) -> decltype(t.SAVE(ar))
     {
         return t.SAVE(ar);
     }
 
-    template <class Archive, class T> inline static
-    auto load(Archive& ar, T& t) -> decltype(t.LOAD(ar))
+    template <class Archive, class Type> inline static
+    auto load(Archive& ar, Type& t) -> decltype(t.LOAD(ar))
     {
         return t.LOAD(ar);
     }
