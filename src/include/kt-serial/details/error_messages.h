52be71e288c3eab54b687dbe20bdb441d5831012
Dmitry Kochetov <kochetov6969@outlook.com>
2021-03-28 12:14:05 +0700

    Error message macros

---
+++ src/include/kt-serial/details/error_messages.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define KTSERIAL_ERROR_NO_HANDLERS_FOUND                                     \
+"Error: matching output serialization handlers not found"
+
+#define KTSERIAL_ERROR_AMBIGUOUS_HANDLERS_FOUND                              \
+"Error: more than one matching output handler found"
