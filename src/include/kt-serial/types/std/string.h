8d53b7ba0393569658ba8e3924129e6e5db7cc5a
Dmitry Kochetov <kochetov6969@outlook.com>
2021-04-10 17:50:06 +0700

    std::string deserialization support

--- src/include/kt-serial/types/std/string.h
+++ src/include/kt-serial/types/std/string.h
@@ -7,8 +7,6 @@
 #include "kt-serial/macros.h"
 
 namespace KtSerial {
-using namespace Traits;
-
 /**
  * Перегрузка функции для сериализации строк стандартной библиотеки:
  * string, wstring, u8string и т.д.
@@ -17,11 +15,20 @@ using namespace Traits;
  * @param str сериализуемая строка
  */
 template <class CharT, class TraitsT, class AllocT, class Archive,
-          EnableIf<HasExactlyOneOutputHandler<DataWrapper<CharT>,
-                                              Archive>::value> = true>
+          Traits::EnableIf<std::is_arithmetic<CharT>::value> = true>
 void KTSERIAL_SAVE_FUNCTION(
     Archive& ar, const std::basic_string<CharT, TraitsT, AllocT>& str) {
     ar << makeSizeWrapper(str.size())
        << makeDataWrapper(str.data(), str.size() * sizeof(CharT));
+}
+
+template <class CharT, class TraitsT, class AllocT, class Archive,
+          Traits::EnableIf<std::is_arithmetic<CharT>::value> = true>
+void KTSERIAL_LOAD_FUNCTION(Archive& ar,
+                            std::basic_string<CharT, TraitsT, AllocT>& str) {
+    SizeType size;
+    ar >> makeSizeWrapper(size);
+    str.resize(size);
+    ar >> makeDataWrapper(const_cast<CharT*>(str.data()), size * sizeof(CharT));
 }
 } // namespace KtSerial
