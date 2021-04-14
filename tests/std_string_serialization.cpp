624f2dd5a6e44d83c0b1ffa8b551747aec67acd5
bachmak <kochetov6969@outlook.com>
2021-04-14 16:10:50 +0700

    Tests for std::string I/O serialization with test function

--- tests/std_string_serialization.cpp
+++ tests/std_string_serialization.cpp
@@ -1,50 +1,31 @@
 #include <gtest/gtest.h>
 
-#include <algorithm>
-#include <sstream>
 #include <string>
 
-#include "kt-serial/archives/binary_istream_archive.h"
-#include "kt-serial/archives/binary_ostream_archive.h"
 #include "kt-serial/types/std/string.h"
-
-template <class CharT> void testString(const std::basic_string<CharT>& str) {
-    std::stringstream stream;
-    {
-        KtSerial::BinaryOstreamArchive oa(stream);
-        oa << str;
-    }
-
-    std::basic_string<CharT> newStr;
-    {
-        KtSerial::BinaryIstreamArchive ia(stream);
-        ia >> newStr;
-    }
-
-    EXPECT_EQ(str, newStr);
-}
+#include "test_helpers/test_functions.h"
 
 TEST(StdStringSerialization, CharString) {
-    testString(std::string("  abcdefgh  "));
-    testString(std::string(""));
-    testString(std::string("a"));
+    testBinaryIOSerialization(std::string("  abcdefgh  "));
+    testBinaryIOSerialization(std::string(""));
+    testBinaryIOSerialization(std::string("a"));
 }
 
 TEST(StdStringSerialization, WideCharString) {
-    testString(std::wstring(L" fsdfsd "));
-    testString(std::wstring(L" алдкгь "));
-    testString(std::wstring(L""));
-    testString(std::wstring(L"Я"));
+    testBinaryIOSerialization(std::wstring(L" fsdfsd "));
+    testBinaryIOSerialization(std::wstring(L" алдкгь "));
+    testBinaryIOSerialization(std::wstring(L""));
+    testBinaryIOSerialization(std::wstring(L"Я"));
 }
 
 TEST(StdStringSerialization, Char32String) {
-    testString(std::u32string(U"  ав  `ыук  "));
-    testString(std::u32string(U""));
-    testString(std::u32string(U" "));
+    testBinaryIOSerialization(std::u32string(U"  ав  `ыук  "));
+    testBinaryIOSerialization(std::u32string(U""));
+    testBinaryIOSerialization(std::u32string(U" "));
 }
 
 TEST(StdStringSerialization, Char16String) {
-    testString(std::u16string(u"  ав  `ыук  "));
-    testString(std::u16string(u""));
-    testString(std::u16string(u" "));
+    testBinaryIOSerialization(std::u16string(u"  ав  `ыук  "));
+    testBinaryIOSerialization(std::u16string(u""));
+    testBinaryIOSerialization(std::u16string(u" "));
 }
