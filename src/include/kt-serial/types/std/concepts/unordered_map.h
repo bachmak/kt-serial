9c66b2f15b3d10f32c2d54472c0bea4ccb609c47
Дмитрий Кочетов <kochetov6969@outlook.com>
2021-05-06 13:50:54 +0300

    std::unordered_map I/O serialization added

---
+++ src/include/kt-serial/types/std/concepts/unordered_map.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "kt-serial/types/common.h"
+
+namespace KtSerial {
+namespace Concepts {
+/**
+ * @brief Обобщенная перегрузка функции для сериализации неупорядоченных
+ * ассоциативных контейнеров стандартной библиотеки, реализующих структуру типа
+ * словарь (unordered_map, unordered_multimap).
+ *
+ * @tparam Archive тип выходного архива
+ * @tparam UnorderedMap тип сериализуемого контейнера (std::unordered_map или
+ * std::unordered_multimap)
+ * @param ar ссылка на выходной архив
+ * @param map ссылка на сериализуемый контейнер
+ */
+template <class Archive, class UnorderedMap>
+void saveUnorderedMap(Archive& ar, const UnorderedMap& map) {
+    ar << makeSizeWrapper(map.size()) << makeSizeWrapper(map.bucket_count());
+
+    for (const auto& item : map) {
+        auto wrapper = makeKeyValueWrapper(item.first, item.second);
+        ar << wrapper;
+    }
+}
+
+/**
+ * @brief Обобщенная перегрузка функции для десериализации неупорядоченных
+ * ассоциативных контейнеров стандартной библиотеки, реализующих структуру типа
+ * словарь (unordered_map, unordered_multimap).
+ *
+ * @tparam Archive тип входного архива
+ * @tparam UnorderedMap тип десериализуемого контейнера (std::unordered_map или
+ * std::unordered_multimap)
+ * @param ar ссылка на входной архив
+ * @param map ссылка на десериализуемый контейнер
+ */
+template <class Archive, class UnorderedMap>
+void loadUnorderedMap(Archive& ar, UnorderedMap& map) {
+    SizeType size, bucketCount;
+    ar >> makeSizeWrapper(size) >> makeSizeWrapper(bucketCount);
+
+    map.clear();
+    map.rehash(bucketCount);
+
+    for (SizeType i = 0; i < size; i++) {
+        typename UnorderedMap::key_type key;
+        typename UnorderedMap::mapped_type value;
+        auto wrapper = makeKeyValueWrapper(key, value);
+        ar >> wrapper;
+        map.insert(std::make_pair(std::move(key), std::move(value)));
+    }
+}
+} // namespace Concepts
+} // namespace KtSerial
