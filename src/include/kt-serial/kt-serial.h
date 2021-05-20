3472b14abc1eaed8809f3be4865e77c4f1ff5611
bachmak <kochetov6969@outlook.com>
2021-05-20 13:14:24 +0700

    Common files to be included by user

---
+++ src/include/kt-serial/kt-serial.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "kt-serial/archives/binary_ostream_archive.h"
+#include "kt-serial/archives/binary_istream_archive.h"
+
+#include "kt-serial/types/std.h"
---
+++ src/include/kt-serial/types/std.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "kt-serial/types/std/array.h"
+#include "kt-serial/types/std/deque.h"
+#include "kt-serial/types/std/forward_list.h"
+#include "kt-serial/types/std/list.h"
+#include "kt-serial/types/std/map.h"
+#include "kt-serial/types/std/pair.h"
+#include "kt-serial/types/std/set.h"
+#include "kt-serial/types/std/string.h"
+#include "kt-serial/types/std/unordered_map.h"
+#include "kt-serial/types/std/unordered_set.h"
+#include "kt-serial/types/std/vector.h"
+
